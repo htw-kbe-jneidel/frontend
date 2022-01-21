8891452812de7478853c9ddf30c73074b5f690a8
Jonathan Neidel <jneidel@jneidel.com>
2022-01-21 14:53:03 +0100

    Refactor and optimize axios calls

--- src/routes.ts
+++ src/routes.ts
@@ -3,53 +3,49 @@ import axios from "axios";
 const router = Router();
 
 const baseUrl = "http://localhost:8000/api";
+async function get( path: string ) {
+  return axios.get( `${baseUrl}${path}` ).then( res => res.data );
+}
+async function post( path: string, data: any ) {
+  return axios.post( `${baseUrl}${path}`, data ).then( res => res.data );
+}
 
-router.get( "/", ( req: any, res ) => {
+router.get( "/", ( req, res ) => {
   return res.render( "landing" );
 } );
 
 router.get( "/product/:id", async ( req: any, res ) => {
   const productId = req.params.id;
-  const product = await axios.get( `${baseUrl  }/product?productId=${  productId}` )
-    .then( res => res.data.product );
 
-  const vat = await axios.post( `${baseUrl  }/caculateVat`, {
-    price: product.price, category: product.category,
-  } )
-    .then( res => res.data.vat );
-
-  const storeLocations = await axios.get( `${baseUrl  }/storeLocations` )
-    .then( res => res.data );
-
-  const amounts = await Promise.all( [
-    axios.get( `${baseUrl}/productQuantityAtLocation?productId=${productId}&storeLocationId=${storeLocations[0].storeLocationId}` ).then( res => res.data.amount ),
-    axios.get( `${baseUrl}/productQuantityAtLocation?productId=${productId}&storeLocationId=${storeLocations[1].storeLocationId}` ).then( res => res.data.amount ),
+  const [ product, storeLocations ] = await Promise.all( [
+    get( `/product?productId=${productId}` ).then( d => d.product ),
+    get( `/storeLocations` ),
   ] );
 
-  storeLocations[0].amount = amounts[0];
-  storeLocations[1].amount = amounts[1];
+  const [ vat, amount1, amount2  ] = await Promise.all( [
+    post( `/caculateVat`, {
+      price   : product.price,
+      category: product.category,
+    } ).then( d => d.vat ),
+    get( `/productQuantityAtLocation?productId=${productId}&storeLocationId=${storeLocations[0].storeLocationId}` ).then( d => d.amount ),
+    get( `/productQuantityAtLocation?productId=${productId}&storeLocationId=${storeLocations[1].storeLocationId}` ).then( d => d.amount ),
+  ] );
+
+  storeLocations[0].amount = amount1;
+  storeLocations[1].amount = amount2;
 
   const unavailable = [ null, null ];
-  if ( amounts[0] === 0 && amounts[1] !== 0 ) {
-    const distance = await axios.post( `${baseUrl  }/calculateDistance`, [
-      storeLocations[0].coordinates, storeLocations[1].coordinates,
-    ] )
-      .then( res => res.data );
-    unavailable[0] = distance;
-  } else if ( amounts[1] === 0 && amounts[0] !== 0 ) {
-    const distance = await axios.post( `${baseUrl  }/calculateDistance`, [
-      storeLocations[1].coordinates, storeLocations[0].coordinates,
-    ] )
-      .then( res => res.data );
-    unavailable[1] = distance;
-  }
+  if ( amount1 === 0 && amount2 !== 0 )
+    unavailable[0] = await post( `/calculateDistance`, [ storeLocations[0].coordinates, storeLocations[1].coordinates ] );
+  else if ( amount2 === 0 && amount1 !== 0 )
+    unavailable[1] = await post( `/calculateDistance`, [ storeLocations[1].coordinates, storeLocations[0].coordinates ] );
+
 
   return res.render( "product", { product, vat: ( product.price - vat ).toFixed( 2 ), storeLocations, unavailable } );
 } );
 
 router.get( "/products", async ( req: any, res ) => {
-  const products = await axios.get( `${baseUrl  }/products` )
-    .then( res => res.data );
+  const products = await get( `/products` );
 
   return res.render( "products", { products } );
 } );
@@ -58,11 +54,9 @@ router.get( "/storeLocation/:id", async ( req: any, res ) => {
   const storeLocationId = req.params.id;
 
   const [ storeLocation, products, quantitiesAtLocations ] = await Promise.all( [
-    axios.get( `${baseUrl  }/storeLocation?storeLocationId=${  storeLocationId}` )
-      .then( res => res.data.storeLocation ),
-    axios.get( `${baseUrl  }/products` ).then( res => res.data ),
-    axios.get( `${baseUrl}/productQuantitiesAtLocations` )
-      .then( res => res.data ),
+    get( `/storeLocation?storeLocationId=${storeLocationId}` ).then( d => d.storeLocation ),
+    get( `/products` ),
+    get( `/productQuantitiesAtLocations` ),
   ] );
 
   const productObject = products.reduce( ( acc, cur ) => {
@@ -83,8 +77,7 @@ router.get( "/storeLocation/:id", async ( req: any, res ) => {
 } );
 
 router.get( "/storeLocations", async ( req: any, res ) => {
-  const storeLocations = await axios.get( `${baseUrl  }/storeLocations` )
-    .then( res => res.data );
+  const storeLocations = await get( `/storeLocations` );
 
   return res.render( "storeLocations", { storeLocations } );
 } );
